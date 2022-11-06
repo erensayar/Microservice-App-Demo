30b72afe8426816372cf031c204409bc0d30e2c1
erensayar <erensayar@yandex.com>
2022-11-06 18:38:01 +0300

    kafka

--- mis-campaign-service/src/main/java/com/erensayar/miscampaignservice/MisCampaignServiceApplication.java
+++ mis-campaign-service/src/main/java/com/erensayar/miscampaignservice/MisCampaignServiceApplication.java
@@ -9,11 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
-@EnableBinding(value = {NotificationDtoBinder.class})
 @EnableFeignClients(basePackageClasses = {
     ProductServiceFeignClient.class,
     NotificationServiceFeignClient.class,
