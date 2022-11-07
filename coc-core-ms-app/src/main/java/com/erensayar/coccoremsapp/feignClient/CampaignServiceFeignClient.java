ce417a63c8ee841cb9a6328a632eca103527528c
erensayar <erensayar@yandex.com>
2022-11-07 09:24:08 +0300

    coc-core renamed

--- coc-core-ms-app/src/main/java/com/erensayar/coccoremsapp/feignClient/CampaignServiceFeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.erensayar.coccoremsapp.feignClient;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "MIS-CAMPAIGN-SERVICE", contextId = "CampaignServiceFeignClient")
-public interface CampaignServiceFeignClient extends CampaignServiceBase {
-
-}
